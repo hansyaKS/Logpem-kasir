c814ae425f5082d177690b0a7f475db3b59d0f25
hansyaKS <93463660+hansyaKS@users.noreply.github.com>
2022-03-12 13:44:36 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,85 @@
+import datetime
 
 
-
-from array import array
-
-
+# salutation
 print("ini adalah program kasir\nsilahkan dipilih")
-# print("silahkan dipilih")
+# global variable
 selesai="n"
-
 total = []
 hargaTotal= []
 
+
+#list of item with atribute name and price
 class item:
-    x=["keju","daging","saus","babi","tempe","tahu","pecel","ayam", "cilok","cireng","telur"]
-    x_price=[2, 6, 8, 7, 9, 8, 5, 4, 3, 6, 1]
+    x=["keju","daging","saus","babi","tempe","tahu","pecel","ayam", "cilok","cireng","telur","kentang"]
+    x_price=[2, 6, 8, 7, 9, 8, 5, 4, 3, 6, 1, 2]
 
-def printlist():
-    for i in range(0,len(item.x)):
-        print(i+1,") ",item.x[i],'\t harga=',item.x_price[i])
+
+#function
+
         
-def printlist2():
+def printlist():
+    # print list of item to be selected by 
     print()
-    for a in range(0, 10):
-        for i in range(0+a,len(item.x),10):
-            print(i+1,") ",item.x[i],'\t harga=',item.x_price[i],"\t",end="")
+    for i in range(0, 15):
+        for j in range(0+i,len(item.x),15):
+            print(j+1,") ",item.x[j],'\t harga=',item.x_price[j],"\t",end="")
         print()
-       
-
-    
-def printtotal2():
-    a= list(set(total))
-    b=[]
-    for i in range(0, len(a)):
-        check=a[i]
-        for c in range(0,len(item.x)):
-            if check==item.x[c]:
-                b.append(item.x_price[c])
-    for i in range(0,len(a)):
-        print(a[i],"\tharga: ",b[i], "\t jumlah barang: ", total.count(a[i]),
-        "\t jumlah harga: ",b[i]*total.count(a[i]))
 
 def printtotal():
-    print({item:total.count(item) for item in total})
-    print ("harga total=\t",sum(hargaTotal),".000")
+    a= list(set(total))
+    b=[]
+    # sorting item name in list a to match the price at item.x_price
+    for i in range(0, len(a)):
+        check=a[i]
+        for j in range(0,len(item.x)):
+            if check==item.x[j]:
+                b.append(item.x_price[j])
 
-while selesai!="y":
-    
-    akhir = "n"
-    while akhir != "y":
-        printlist2()
-        masukanLi= list(map(int,input("\nmasukkan angka \t:").strip().split()))
+    # printing list of item to buy, its price and total price
+    print("\n            Hal yang Dipilih             \nitem\tharga\tjumlah\t jumlahtotal")
+   
+    for i in range(0,len(a)):
+        print(a[i],"\t|",b[i], "\t|", total.count(a[i]),
+        "\t|",b[i]*total.count(a[i]))
+    print ("\nharga total=\t",sum(hargaTotal),".000")
 
-        for i in range(0,len(masukanLi)):
-            masukan= masukanLi[i]
-            for a in range(0,len(item.x)):
-                if masukan ==int(a+1):
-                    total.append(item.x[a])
-                    hargaTotal.append(item.x_price[a])
-        akhir= str (input("\n apakah anda sudah selesai memilih? (y/n)\t:"))
-    
+
+def struckconstruct():
+    print("\n\n\n\tStruk Pembayaran\nSistem pemesanan makananan LOGPEM B\n\nDate and time :",datetime.datetime.now() )
     printtotal()
-    selesai=input("apakah sudah sesuai  (y/n)")
-    if selesai =="n":
-        if input("apakah mau reset (y/n)")== "y":
-            total = []
-            hargaTotal= int()
+    print("\nTerimakasih sudah melakukan transaksi\nkami tunggu kedatangan anda kembali\n\nlayanan pengaduan : hansa@mail.ugm.ac.id")
+    # print(datetime.datetime.now())
 
-printtotal2()
+
+
+# ****mainloop*******
+  
+# interface for selecting item 
+while selesai != "y":
+    printlist()
+    #user inputing corespondent item then convert into list
+    masukanLi= list(map(int,input("\nmasukkan angka \t:").strip().split()))
+    #storing list of item to buy to total and hargaTotal variable
+    for i in range(0,len(masukanLi)):
+        masukan= masukanLi[i]
+        for j in range(0,len(item.x)):
+            if masukan ==int(j+1):
+                total.append(item.x[j])
+                hargaTotal.append(item.x_price[j])
+
+    # checking order prompt
+    printtotal()
+    # selesai= str (input("\n apakah anda sudah selesai memilih? (y/n)\t: "))
+    if input("\n apakah anda sudah selesai memilih? (y/n)\t: ")=="n":   
+        #reset buylist prompt
+        if input("apakah mau reset (y/n)\t: ")== "y":
+            total = []
+            hargaTotal=[]
+    else:
+        selesai="y"
+
+struckconstruct()
+# printtotal()
 
 # print(*total, sep=", ")
